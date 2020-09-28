0ef6c39e11071d14b5311e5db0d60f856261c942
MrLeii <1728820789@qq.com>
2020-09-28 10:25:28 +0800

    Add files via upload

--- MyLayoutControlView.cpp
+++ MyLayoutControlView.cpp
@@ -13,11 +13,20 @@
 #include "MyLayoutControlView.h"
 
 #include "MyLayoutSet.h"
-#include "d:\vs2010\mylayoutcontrol\gridctrl_src\gridctrl.h"
+#include "GridCtrl_src\GridCtrl.h"
+
+#include "NewCellTypes/GridURLCell.h"
+#include "NewCellTypes/GridCellCombo.h"
+#include "NewCellTypes/GridCellCheck.h"
+#include "NewCellTypes/GridCellNumeric.h"
+#include "NewCellTypes/GridCellDateTime.h"
+#include "NewCellTypes\GridCellButton.h"
+
 
 CMyLayoutSet *mylayoutset;
-
-
+extern CString strRow,strCol;
+BOOL bnSet;
+extern BOOL m_Button;
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
@@ -36,8 +45,7 @@ ON_COMMAND(ID_DelRow, &CMyLayoutControlView::OnDelRow)
 ON_COMMAND(ID_DelCol, &CMyLayoutControlView::OnDelcol)
 ON_COMMAND(ID_Merge, &CMyLayoutControlView::OnMerge)
 ON_COMMAND(ID_Div, &CMyLayoutControlView::OnDiv)
-ON_BN_CLICKED(IDOK, &CMyLayoutControlView::OnBnClickedOk)
-ON_BN_CLICKED(IDC_BUTTON2, &CMyLayoutControlView::OnBnClickedButton2)
+ON_COMMAND(ID_CtrlBtn, &CMyLayoutControlView::OnInsertCtrlbtn)
 END_MESSAGE_MAP()
 
 // CMyLayoutControlView 构造/析构
@@ -71,8 +79,7 @@ void CMyLayoutControlView::OnInitialUpdate()
 {
 	CFormView::OnInitialUpdate();
 
-	//m_Grids.EnableDragAndDrop(TRUE);
-
+	
 	OnVirtualMode();
 
  }
@@ -94,34 +101,36 @@ void CMyLayoutControlView::OnVirtualMode()
 
     if (m_bVirtualMode)
     {
-
-        m_nFixCols = 1;
-	    m_nFixRows = 1;
-
-	    TRY {
-		    m_Grids.SetRowCount(m_nRows);
-		    m_Grids.SetColumnCount(m_nCols);
-		    m_Grids.SetFixedRowCount(m_nFixRows);
-		    m_Grids.SetFixedColumnCount(m_nFixCols);
-
-			m_Grids.SetColumnWidth(0,20);
-			m_Grids.SetRowHeight(0,20);
-			m_Grids.SetEditable(TRUE);
-
-			for(int i = 1,j = 1;i < m_nRows,j < m_nCols;i++,j++)
-			{
-				m_Grids.SetRowHeight(i,200);
-				m_Grids.SetColumnWidth(j,200);
-			}
 	
-	    }
-	    CATCH (CMemoryException, e)
-	    {
-	    	e->ReportError();
-    		return;
-	    }
-        END_CATCH
-    }
+		{
+			m_nFixCols = 1;
+			m_nFixRows = 1;
+
+			TRY {
+				m_Grids.SetRowCount(m_nRows);
+				m_Grids.SetColumnCount(m_nCols);
+				m_Grids.SetFixedRowCount(m_nFixRows);
+				m_Grids.SetFixedColumnCount(m_nFixCols);
+
+				m_Grids.SetColumnWidth(0,20);
+				m_Grids.SetRowHeight(0,20);
+				m_Grids.SetEditable(TRUE);
+
+				for(int i = 1,j = 1;i < m_nRows,j < m_nCols;i++,j++)
+				{
+					m_Grids.SetRowHeight(i,200);
+					m_Grids.SetColumnWidth(j,200);
+				}
+	
+			}
+			CATCH (CMemoryException, e)
+			{
+	    		e->ReportError();
+    			return;
+			}
+			END_CATCH
+		}
+	}
     else
     {
         m_nFixCols = 1;
@@ -142,8 +151,8 @@ void CMyLayoutControlView::OnVirtualMode()
 
 			for(int i = 1,j = 1;i < m_nRows,j < m_nCols;i++,j++)
 			{
-				m_Grids.SetRowHeight(i,600/m_nRows);
-				m_Grids.SetColumnWidth(j,600/m_nCols);
+				m_Grids.SetRowHeight(i,600/(m_nRows-1));
+				m_Grids.SetColumnWidth(j,600/(m_nCols-1));
 				
 			}
 			//填充整个控件
@@ -163,6 +172,22 @@ void CMyLayoutControlView::OnVirtualMode()
 
 void CMyLayoutControlView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
 {
+	CPoint ptCursor;
+	GetCursorPos(&ptCursor);//获取鼠标位置
+	CRect rc;
+
+	GetDlgItem(IDC_CUSTOM_SET)->GetWindowRect(&rc);
+	if (rc.PtInRect(ptCursor))
+	{
+	CMenu menu;
+	menu.LoadMenu(IDR_MENU1);
+	GetCursorPos(&point);
+	ASSERT(menu.GetSafeHmenu());
+	CMenu* pSubMenu = menu.GetSubMenu(0);
+	ASSERT(pSubMenu);
+	pSubMenu->TrackPopupMenu(TPM_LEFTALIGN|TPM_LEFTBUTTON|TPM_RIGHTBUTTON,point.x,point.y,this);
+	menu.Detach();
+	}
 #ifndef SHARED_HANDLERS
 	//theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
 #endif
@@ -268,20 +293,14 @@ void CMyLayoutControlView::OnSize(UINT nType, int cx, int cy)
  //   END_CATCH
 }*/
 
+
+
 void CMyLayoutControlView::OnRButtonUp(UINT nFlags, CPoint point)
 {
 	// TODO: 在此添加消息处理程序代码和/或调用默认值
-
-	CMenu menu;
-	menu.LoadMenu(IDR_MENU1);
-	GetCursorPos(&point);
-	ASSERT(menu.GetSafeHmenu());
-	CMenu* pSubMenu = menu.GetSubMenu(0);
-	ASSERT(pSubMenu);
-	pSubMenu->TrackPopupMenu(TPM_LEFTALIGN|TPM_LEFTBUTTON|TPM_RIGHTBUTTON,point.x,point.y,this);
-	menu.Detach();
 	//CFormView::OnRButtonUp(nFlags, point);
 }
+
 
 
 void CMyLayoutControlView::OnDelRow()
@@ -294,28 +313,32 @@ void CMyLayoutControlView::OnDelRow()
 	int row;
 	for(row = Selection.GetMinRow();row <= Selection.GetMaxRow();row++) 
 	{ 
-		pCell = m_Grids.GetCell(row,1);
-		m_Grids.DeleteRow(row);
-		m_nRows--;
+		//仅当选中整行时删除
+		if(Selection.GetMaxCol() == (m_nCols-1))
+		{
+			pCell = m_Grids.GetCell(row,1);
+			m_Grids.DeleteRow(row);
+			m_nRows--;
+		}
 	}
 		//限制只能删除一行
 		//if(pCell &&(pCell-> GetState() & GVIS_SELECTED)) 
 		//	{
-		//		m_Grids.DeleteRow(row);
-		//		m_nRows--;
-		//		
+		//		m_Grids.DeleteRow(row);	
 		//    } 
 	m_Grids.SetColumnWidth(0,15);
 	m_Grids.SetRowHeight(0,15);
 	for(int i = 1;i < m_nRows;i++)
 	{
-		m_Grids.SetRowHeight(i,600/m_nRows);
+		m_Grids.SetRowHeight(i,600/(m_nRows-1));
 			
 	}
 	//填充整个控件
 	//m_Grids.ExpandToFit();
-
-	m_Grids.Invalidate();
+	//m_Grids.Invalidate();
+	//bSet = FALSE;
+	bnSet = FALSE;
+	m_Grids.Refresh();
 	
 }
 
@@ -330,21 +353,24 @@ void CMyLayoutControlView::OnDelcol()
 
 	for(int col = Selection.GetMinCol();col <= Selection.GetMaxCol();col++) 
 	{ 
-		pCell = m_Grids.GetCell(1,col); 
-		m_Grids.DeleteColumn(col);
-		m_nCols--;
+		if(Selection.GetMaxRow() == (m_nRows-1))
+		{
+			pCell = m_Grids.GetCell(1,col); 
+			m_Grids.DeleteColumn(col);
+			m_nCols--;
+		}
 	} 
+	m_Grids.SetRowCount(m_nRows);
+	m_Grids.SetColumnCount(m_nCols);
 	m_Grids.SetColumnWidth(0,15);
 	m_Grids.SetRowHeight(0,15);
 	for(int i = 1;i < m_nCols;i++)
 	{
-		m_Grids.SetColumnWidth(i,600/m_nCols);
+		m_Grids.SetColumnWidth(i,600/(m_nCols-1));
 			
 	}
-	//填充整个控件
-	//m_Grids.ExpandToFit();
-
-	m_Grids.Invalidate();
+	bnSet = FALSE;
+	m_Grids.Refresh();
 }
 
 //合并
@@ -362,48 +388,14 @@ void CMyLayoutControlView::OnDiv()
 	m_Grids.UnMergeSelectedCells();
 }
 
-
-
-void CMyLayoutControlView::OnBnClickedOk()
-{
-	// TODO: 在此添加控件通知处理程序代码
-	extern CString strRow,strCol;
-	
-	m_nRows = _ttoi(strRow);
-	m_nCols = _ttoi(strCol);
-	UpdateData(FALSE);
-	TRY {
-        m_Grids.SetRowCount(m_nRows);
-		m_Grids.SetColumnCount(m_nCols);
-		for(int i = 1,j = 1;i < m_nRows,j < m_nCols;i++,j++)
-		{
-			m_Grids.SetRowHeight(i,600/m_nRows);
-			m_Grids.SetColumnWidth(j,600/m_nCols);
-			
-		}
-		//填充整个控件
-		m_Grids.ExpandToFit();
-    }
-	CATCH (CMemoryException, e)
-	{
-		e->ReportError();
-		return;
-	}
-    END_CATCH
-
-	UpdateData(FALSE);
-
-}
-
-
-void CMyLayoutControlView::OnBnClickedButton2()
+/*void CMyLayoutControlView::OnBnClickedButton2()
 {
 	// TODO: 在此添加控件通知处理程序代码
 	//UpdateData();
-	extern CString strRow,strCol;
-
-	m_nRows = _ttoi(strRow);
-	m_nCols = _ttoi(strCol);
+	
+	//表头不算行
+	m_nRows = _ttoi(strRow) + 1;
+	m_nCols = _ttoi(strCol) + 1;
 	UpdateData(FALSE);
 	TRY {
         m_Grids.SetRowCount(m_nRows);
@@ -412,8 +404,8 @@ void CMyLayoutControlView::OnBnClickedButton2()
 		m_Grids.SetRowHeight(0,15);
 		for(int i = 1,j = 1;i < m_nRows,j < m_nCols;i++,j++)
 		{
-			m_Grids.SetRowHeight(i,600/m_nRows);
-			m_Grids.SetColumnWidth(j,600/m_nCols);
+			m_Grids.SetRowHeight(i,600/(m_nRows-1));
+			m_Grids.SetColumnWidth(j,600/(m_nCols-1));
 			
 		}
 		//填充整个控件
@@ -427,4 +419,67 @@ void CMyLayoutControlView::OnBnClickedButton2()
     END_CATCH
 
 	UpdateData(FALSE);
+}*/
+
+
+BOOL CMyLayoutControlView::PreTranslateMessage(MSG* pMsg)
+{
+	//设置单元格不可编辑
+	m_Grids.EnableDragAndDrop(FALSE);
+	m_Grids.SetEditable(FALSE);
+
+	if(bnSet)
+	{
+		m_nRows = _ttoi(strRow) + 1;
+		m_nCols = _ttoi(strCol) + 1;
+		m_Grids.SetRowCount(m_nRows);
+		m_Grids.SetColumnCount(m_nCols);
+		m_Grids.SetColumnWidth(0,15);
+		m_Grids.SetRowHeight(0,15);
+		for(int i = 1,j = 1;i < m_nRows,j < m_nCols;i++,j++)
+		{
+			m_Grids.SetRowHeight(i,600/(m_nRows-1));
+			m_Grids.SetColumnWidth(j,600/(m_nCols-1));
+			
+		}
+		m_Grids.Refresh();
+	}
+	/*填充整个控件
+	m_Grids.ExpandToFit();*/
+	
+	return CView::PreTranslateMessage(pMsg);
+}
+
+
+
+
+void CMyLayoutControlView::OnInsertCtrlbtn()
+{
+	// TODO: 在此添加命令处理程序代码
+	//CCellRange Selection = m_Grids.GetSelectedCellRange(); 
+	//if(!m_Grids.IsValid(Selection)) 
+ //   return   ; 
+	int n;
+
+	//CRect  rect_button(50,50,100,100);//控制按钮位置、大小 ;
+	//m_button.Create(_T("hello"),WS_CHILD|WS_VISIBLE|WS_BORDER,rect_button,this,IDC_BUTTON1);
+	//GetDlgItem(IDC_BUTTON1)->SetWindowPos(GetDlgItem(IDC_CUSTOM_SET), 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+	//GetDlgItem(IDC_BUTTON1)->ShowWindow(SW_HIDE);
+	//m_button.ShowWindow(SW_SHOW);
+
+/*
+	switch(n)
+	{
+	}*/
+	m_Grids.SetCellType(1,1, RUNTIME_CLASS(CGridCellButton));
+
+	CGridCellButton* pCell11 = (CGridCellButton*) m_Grids.GetCell(1,1);
+	m_Grids.RedrawCell(1,1);
+
+	m_Grids.SetCellType(2,1, RUNTIME_CLASS(CGridCellButton));
+
+	CGridCellButton* pCell21 = (CGridCellButton*) m_Grids.GetCell(2,1);
+	m_Grids.RedrawCell(2,1);
+
+	
 }
